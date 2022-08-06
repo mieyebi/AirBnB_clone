300c9eb963fd8cd1a5b3ef2ec62f4a520c57e908
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-08-06 21:22:17 +0000

    add tests folder

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""test module for Amenity class"""
+
+
+from models.amenity import Amenity
+import unittest
+
+
+class TestUser(unittest.TestCase):
+    """test User"""
+
+    def test_class_name(self):
+        """test class name"""
+        self.assertEqual("Amenity", Amenity.class_name)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""test module for User class"""
+
+
+from models.user import User
+import unittest
+
+
+class TestUser(unittest.TestCase):
+    """test User"""
+
+    def test_class_name(self):
+        """test class name"""
+        self.assertEqual("User", User.class_name)
+
+
+if __name__ == "__main__":
+    unittest.main()
