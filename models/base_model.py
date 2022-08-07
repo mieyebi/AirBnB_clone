6c0fdd41a37bfff6ba266a1ae46d1e7a7e4d41ab
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-08-07 22:04:56 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
         storage.save()
 
     def __str__(self):
-        """string representation of instance"""
+        """string representation of an instance"""
         string = "[{}] ({}) {}".format(self.__class__.__name__,
                                        self.id,
                                        self.__dict__)
