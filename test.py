e40613a6d6aebe464078eeae38775dc9f41677df
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-08-08 10:16:40 +0000

    remove gitignore

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-from models import storage
-from models.base_model import BaseModel
+"""Doc
+"""
+from models.tmp_base_model import *
+from models.tmp_base_model import BaseModel
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
 
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
+class BaseModel(BaseModel):
+    """Doc
+    """
+
+    def save(self):
+        """Doc
+        """
+        pass
